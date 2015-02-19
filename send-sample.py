2bd82f436d4e8400f9214d95aa55cb5d7916f440
Lasse Karstensen <lasse.karstensen@gmail.com>
2015-02-19 09:46:53 +0100

    Use format defined by sender.

--- send-sample.py
+++ send-sample.py
@@ -8,9 +8,16 @@ if __name__ == "__main__":
     s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     dest = ('255.255.255.255', 1235)
     print "Sending samples to %s port %s." % dest
-    data = "input1=244 input2=0 input3=233 input4=1\n"
+
+    #powerhaus.hackeriet.no ticks/minute: 4.5 12.1 3.4 0.0 11.2 23.32 CTpower: 1234 4321 12315 123 0 31213
+    ticksamples = "4.5 12.1 3.4 0.0 11.2 23.32".split()  # LDR blinks.
+    powersamples = "1234 4321 12315 123 0 31213".split()  # current sensors
+
     while True:
-        s.sendto(data, dest)
+        msg = ["powerhaus.hackeriet.no"]
+        msg += ["ticks/minute: "] + ticksamples
+        msg += ["TPower:"] + powersamples
+        s.sendto(" ".join(msg), dest)
         sys.stdout.write(".")
         sys.stdout.flush()
         time.sleep(0.5)
