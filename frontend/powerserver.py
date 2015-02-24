9687098bb7b7f16d9b8361204e3a22200295f4b8
Lasse Karstensen <lasse.karstensen@gmail.com>
2015-02-24 21:40:03 +0100

    cors

--- frontend/powerserver.py
+++ frontend/powerserver.py
@@ -44,6 +44,7 @@ class HTTPRequestHandler(SimpleHTTPRequestHandler):
             body = json.dumps(readings, indent=2)
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
+            self.send_header("Access-Control-Allow-Origin", "*")
             self.send_header("Content-Length", len(body))
             self.end_headers()
             self.wfile.write(body)
