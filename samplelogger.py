86c8a3f84e921928b9435fe0e6dc5bc82a089157
Lasse Karstensen <lasse.karstensen@gmail.com>
2015-07-21 23:17:13 +0200

    Rewrite to be a pcap logger.

--- samplelogger.py
+++ samplelogger.py
@@ -1,31 +1,21 @@
 #!/usr/bin/env python
 """
-A small server process that listens for UDP broadcast,
-filters the incoming data and provide a JSON web service
-with the results.
+A small pcap service that listens for the malformed UDP broadcasts
+sent by the power monitor, and writes a data file to disk.
 
 Used for graphing power usage.
 
-Author: Lasse Karstensen <lasse.karstensen@gmail.com>, February 2015.
+Author: Lasse Karstensen <lasse.karstensen@gmail.com>, July 2015.
 """
-import threading
+
+import os
 import json
-import random
-import socket
-import BaseHTTPServer
-import SocketServer
-
+import sys
 from datetime import datetime
-
+from pprint import pprint, pformat
 import pcapy
 
-from SocketServer import UDPServer, DatagramRequestHandler
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from pprint import pprint, pformat
-from sys import argv, stdout
-
-# { "sensorid": [ 1.1, 1.2, 0.9, 1.1 ], }
-readings = {}
+outputfp = None
 
 def parse_sample(payload):
     """
@@ -51,88 +41,39 @@ def parse_sample(payload):
     return r
 
 def pkt_cb(pkthdr, data):
-    global readings
-    print data
+    assert outputfp is not None
+
     # best check ever.
     if "zool" not in data:
         return
     ts = datetime.fromtimestamp(pkthdr.getts()[0])
-    print "%s\t" % ts,
-    sample = parse_sample(data)
     try:
+        sample = parse_sample(data)
         #print " ".join(["%s=%.3f" % (x[0], x[1]) for x in sample])
-        print "\t".join(["%.2f" % (x[1]) for x in sample])
+        outputfp.write("%s\t" % ts)
+        outputfp.write("\t".join(["%.2f" % (x[1]) for x in sample]) + "\n")
     except AttributeError:
-        print "INVALID packet data: %s" % pformat(sample)
-    stdout.flush()
-#    pprint(sample)
+        outputfp.write("%s\t" % ts)
+        outputfp.write("INVALID packet data: %s\n" % pformat(sample))
+    outputfp.flush()
        
-
-class SampleHandler(DatagramRequestHandler):
-    def handle(self):
-        global readings
-        payload = self.rfile.read()
-        pprint(payload)
-
-        for k in readings.keys():
-            if len(readings[k]) > 20:
-                readings[k].pop(0)
-
-class HTTPRequestHandler(SimpleHTTPRequestHandler):
-    def log_message(self, *args, **kwargs):
-        pass
-
-    def do_GET(self):
-        if (self.path == "/"):
-            msg = "See /readings.json, please.\n"
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.send_header("Content-Length", len(msg))
-            self.end_headers()
-            self.wfile.write(msg)
-        elif (self.path == "/readings.json"):
-            body = json.dumps(readings, indent=4)
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.send_header("Access-Control-Allow-Origin", "*")
-            self.send_header("Content-Length", len(body))
-            self.end_headers()
-            self.wfile.write(body)
-        else:
-            msg = "400 Bad Request"
-            self.send_response(400)
-            self.send_header("Content-type", "text/plain")
-            self.send_header("Content-Length", len(msg))
-            self.end_headers()
-            self.wfile.write(msg)
-
-class ThreadedHTTPServer(SocketServer.ThreadingMixIn,
-                         BaseHTTPServer.HTTPServer):
-    """Handle requests in a separate thread."""
-
-    def handle_error(self, request, client_address):
-        if 0:
-            import traceback
-            traceback.print_exc()
+def handle(self):
+    global readings
+    payload = self.rfile.read()
+    pprint(payload)
+    for k in readings.keys():
+        if len(readings[k]) > 20:
+            readings[k].pop(0)
 
 if __name__ == '__main__':
-    if "-d" in argv:
-        import daemon
-        daemon.daemonize("/var/tmp/powerserver.pid")
+    if os.geteuid() != 0:
+        print >>sys.stderr, "ERROR: Need to be root to run pcap."
+        exit(1)
 
-    p = pcapy.open_live("eth0", 1500, False, 0)
+    outputfp = sys.stdout
+    if len(sys.argv) > 1:
+        outputfp = open(sys.argv[1], "a")
+
+    p = pcapy.open_live("eth0", 1500, True, 0)
     p.setfilter("udp and port 54321")
     p.loop(-1, pkt_cb)
-
-    #us = UDPServer(("10.0.130.255", 54321), SampleHandler)
-    #us = UDPServer(("10.0.130.255", 54321), SampleHandler)
-    #us.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    #t = threading.Thread(target=us.serve_forever)
-    #t.setDaemon = True
-    #t.start()
-
-    BaseHTTPServer.allow_reuse_address = True
-    SocketServer.TCPServer.address_family = socket.AF_INET6
-    SocketServer.TCPServer.allow_reuse_address = True
-    hp = ThreadedHTTPServer(("", 8088), HTTPRequestHandler)
-    hp.serve_forever()
